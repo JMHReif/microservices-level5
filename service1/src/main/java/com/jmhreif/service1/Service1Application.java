0a484d43437b8a475271443a85a0002b06803725
Jennifer Reif <jennifer.reif@neo4j.com>
2022-01-10 13:39:10 -0600

    Success - fixed by adding annotation to class and improving syntax

--- service1/src/main/java/com/jmhreif/service1/Service1Application.java
+++ service1/src/main/java/com/jmhreif/service1/Service1Application.java
@@ -1,8 +1,6 @@
 package com.jmhreif.service1;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,16 +21,15 @@ import java.time.Duration;
 public class Service1Application {
 	@Bean
 	CommandLineRunner clr(BookRepository repo) {
-		return args -> {
-			repo.deleteAll()
+		return args -> repo.deleteAll()
 					.thenMany(Flux.just(
-							new Book("1", "The Lord of the Rings: The Return of the King", "J.R.R. Tolkien"),
-							new Book("2", "Harry Potter and the Prisoner of Azkaban", "J.K. Rowling"),
-							new Book("3", "Star Wars: The Truce at Bakura", "Kathy Tyers"),
-							new Book("4", "The Phoenix Project", "Gene Kim/Kevin Behr/George Spafford")))
+							new Book("The Lord of the Rings: The Return of the King", "J.R.R. Tolkien"),
+							new Book("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling"),
+							new Book("Star Wars: The Truce at Bakura", "Kathy Tyers"),
+							new Book("The Phoenix Project", "Gene Kim/Kevin Behr/George Spafford")))
 					.flatMap(repo::save)
+					.log()
 					.subscribe();
-		};
 	}
 
 	public static void main(String[] args) {
@@ -44,7 +41,7 @@ public class Service1Application {
 @AllArgsConstructor
 @RequestMapping("/db")
 class BookController {
-	BookRepository bookRepository;
+	private final BookRepository bookRepository;
 
 	@GetMapping("/books")
 	Flux<Book> getBooks() { return bookRepository.findAll(); }
@@ -53,12 +50,14 @@ class BookController {
 interface BookRepository extends ReactiveCrudRepository<Book, String> {
 }
 
+@Data
 @Document
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 class Book {
 	@Id
 	private String bookId;
+	@NonNull
 	private String title;
+	@NonNull
 	private String author;
 }
