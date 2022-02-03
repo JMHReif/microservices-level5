38c93a0d4f1ab53dbb9cb32ecaa2b0c278ac0e96
Jennifer Reif <jennifer.reif@neo4j.com>
2022-02-03 15:49:07 -0600

    Initial commit

--- service1/src/main/java/com/jmhreif/service1/Service1Application.java
+++ service1/src/main/java/com/jmhreif/service1/Service1Application.java
@@ -1,36 +1,20 @@
 package com.jmhreif.service1;
 
-import lombok.*;
-import org.springframework.boot.CommandLineRunner;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-
 @SpringBootApplication
 public class Service1Application {
-	@Bean
-	CommandLineRunner clr(BookRepository repo) {
-		return args -> repo.deleteAll()
-					.thenMany(Flux.just(
-							new Book("The Lord of the Rings: The Return of the King", "J.R.R. Tolkien"),
-							new Book("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling"),
-							new Book("Star Wars: The Truce at Bakura", "Kathy Tyers"),
-							new Book("The Phoenix Project", "Gene Kim/Kevin Behr/George Spafford")))
-					.flatMap(repo::save)
-					.log()
-					.subscribe();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service1Application.class, args);
@@ -54,9 +38,9 @@ interface BookRepository extends ReactiveCrudRepository<Book, String> {
 @Document
 class Book {
 	@Id
-	private String bookId;
+	private String bookID;
 	@NonNull
 	private String title;
 	@NonNull
-	private String author;
+	private String authors;
 }
