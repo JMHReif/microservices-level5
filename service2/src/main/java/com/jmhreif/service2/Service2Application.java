f96ac1614a3edea9a0b576468bbc44978864849f
Jennifer Reif <jennifer.reif@neo4j.com>
2022-03-29 16:12:38 -0500

    Fix: expose service2 as rest client

--- service2/src/main/java/com/jmhreif/service2/Service2Application.java
+++ service2/src/main/java/com/jmhreif/service2/Service2Application.java
@@ -3,6 +3,7 @@ package com.jmhreif.service2;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,16 +27,19 @@ public class Service2Application {
 
 	@Bean
 	WebClient client() {
-		return WebClient.create("http://localhost:8080");
+		return WebClient.create("http://localhost:8081");
 	}
 
 }
 
 @Component
+@RestController
 @AllArgsConstructor
+@RequestMapping("/goodreads")
 class BookController {
 	private final WebClient client;
 
+	@GetMapping
 	Flux<Book> getBooks() {
 		return client.get()
 				.uri("/db/books")
@@ -42,8 +48,9 @@ class BookController {
 	}
 }
 
+@Data
 class Book {
-	private String bookID;
+	private String book_id;
 	private String title;
-	private String authors;
+	private String format, isbn, isbn13, edition_information;
 }
