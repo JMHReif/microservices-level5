2cfd8b86bd0a6731f04c40d92b149f7f8e82f82e
Jennifer Reif <jennifer.reif@neo4j.com>
2022-04-21 11:14:37 -0500

    Optimize imports for service2

--- service2/src/main/java/com/jmhreif/service2/Service2Application.java
+++ service2/src/main/java/com/jmhreif/service2/Service2Application.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
